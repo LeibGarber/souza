20aaa2cb3e68d3710784c47e1dfae2b5b06ebd33
steve garber <steve.l.garber@gmail.com>
2018-09-12 03:08:37 +0300

    built contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
+import "./Contact.css";
+import Gps from "./imgs/gpsIcon.svg.js";
 class Contact extends Component {
   render() {
     return (
       <div>
-        <h1> This is a blank contact view component</h1>
+        <section className="hero contactFormHero">
+          <div
+            className="background-image"
+            style={{
+              backgroundImage:
+                "url(https://upload.wikimedia.org/wikipedia/commons/b/b7/Tzfat_sunset_%282063020598%29.jpg)"
+            }}
+          />
+          <div className="contactGrid">
+            <h1 className="contactHeader">CONTACT</h1>
+            <p className="contactBlurb">
+              Interested in working together? Please be in touch with some info
+              about your project and I will get back to you as soon as I can.
+              Please allow a few days for me to respond.{" "}
+            </p>
+            <div className="headquarters">
+              <h4>
+                <Gps /> Headquarters
+              </h4>
+              <p>Shim'on Bar Yokhai St 51, Tsfat</p>
+            </div>
+            <div className="emailAddy">
+              <h4>Email</h4>
+              <p>solomonsouza@someplace.com</p>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }

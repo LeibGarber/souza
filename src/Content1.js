85c670e1c93e3bede369a961c2f0ab17decde3ca
steve garber <steve.l.garber@gmail.com>
2018-09-19 23:59:41 +0300

    updated gallery, removed grid and refactored w/ flex

--- src/Content1.js
+++ src/Content1.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import "./Content.css";
 import Nissim from "./imgs/nissim.jpg";
 import Nachlaot from "./imgs/nachlaot.jpg";
+import Battle from "./imgs/battle.jpg";
+import Faces from "./imgs/faces.jpg";
+import Prayer from "./imgs/prayer.jpg";
+import kohenGadol from "./imgs/kohenGadolFix.jpg";
+import Adam from "./imgs/adam.jpg";
+import Fish from "./imgs/fish.jpg";
+import Lines from "./imgs/linesFix.jpg";
+import Paper from "./imgs/paperFix.jpg";
 class Content1 extends Component {
   render() {
     return (
@@ -10,38 +18,45 @@ class Content1 extends Component {
 
         <div className="GalleryGrid">
           <div className="GalleryPic small">
-            <img src={Nissim} />
-          </div>
-
-          <div className="GalleryPic large">
-            <img src={Nachlaot} />
+            <img src={Battle} />
           </div>
 
           <div className="GalleryPic small1">
-            <img src={Nissim} />
+            <img src={Faces} />
           </div>
 
-          <div className="GalleryPic medium">
-            <img src={Nissim} />
+          <div className="GalleryPic small2">
+            <img src={kohenGadol} />
+          </div>
+
+          <div className="GalleryPic small3">
+            <img src={Adam} />
           </div>
 
           <div className="signature">
             <h1>SOUZA</h1>
           </div>
 
-          <div className="GalleryPic xl">
-            <img src={Nachlaot} />
+          <div className="GalleryPic small4">
+            <img src={Fish} />
+          </div>
+
+          <div className="GalleryPic small5">
+            <img src={Lines} />
+          </div>
+
+          <div className="GalleryPic small6">
+            <img src={Paper} />
+          </div>
+
+          <div className="GalleryPic small7">
+            <img src={Prayer} />
           </div>
         </div>
 
         <section className="features">
-          <h3 className="title">Some recent work</h3>
-          <p>
-            כל הדברים שבגמרא הבבלי חייבין כל ישראל ללכת בהם וכופין כל עיר ועיר
-            וכל מדינה ומדינה לנהוג בכל המנהגות שנהגו חכמי הגמרא
-          </p>
           <br />
-          <h1> This is a blank content view component</h1>
+
           <br />
         </section>
       </div>
